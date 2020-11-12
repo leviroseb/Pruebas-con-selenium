1f3159d0792cb507e57a6bf2d96a65b1be63cafe
Jhon Flores Pacheco <38104862+leviroseb@users.noreply.github.com>
2020-11-11 20:30:28 -0500

    Update GoogleSearchTest.java

--- GoogleSearchTest.java
+++ GoogleSearchTest.java
@@ -18,24 +18,36 @@ public class GoogleSearchTest {
 	
 	@Before
 	public void setUp() {
+		//carga el driver
 		System.setProperty("webdriver.chrome.driver","./src/test/resources/chromedriver/chromedriver.exe");
+		//Ahora creamos el objeto WebDriver
 		driver = new ChromeDriver();
+		//Abrimos una instancia del navegador
 		driver.manage().window().maximize();
+		//dirección url que queremos abrir
 		driver.get("https://www.google.com/");
 	}
 	
 	@Test
 	public void testGooglePage() {
+		//obtiene la caja de búsqueda de google
 		WebElement searchbox=driver.findElement(By.name("q"));
+		//Se limpia la caja de búsqueda
 		searchbox.clear();
+		//Contenido que se desea buscar
 		searchbox.sendKeys("quality-stream Introduccion a la automatizacion de pruebas");
+		//Se envía el texto
 		searchbox.submit();
+		//tiempo de carga de los resultados de búsqueda
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		//el contenido de búsqueda debe ser igual al título de la página de  resultado
 		assertEquals("quality-stream Introduccion a la automatizacion de pruebas - Google Search",driver.getTitle());
 	}
 	
 	@After
 	public void tearDown() {
+		
+		//Se cierra el navegador una ve que se completa la prueba
 		driver.quit();
 	}
 }
